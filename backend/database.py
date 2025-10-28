01d68bcbcc76bc8a52430b61dd5b91ccca8179ab
-@dj4690928- <dj4690928@gmail.com>
2025-10-28 17:29:31 +0530

    first commit1

--- backend/database.py
+++ backend/database.py
@@ -2,16 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# MySQL connection
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:darshan08@localhost/expense_db"
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:oGuPa3RBkQciy1Cd6mzH5ltYlL1s109r@dpg-d40amff5r7bs73aanrlg-a:5432/expense_db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./expense.db"
 
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency for DB session
 def get_db():
     db = SessionLocal()
     try:
--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float, Date
-from .database import Base
+from backend.database import Base
 
 class Expense(Base):
     __tablename__ = "expenses"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100))          # Expense title (must exist in DB)
+    title = Column(String(255))
     amount = Column(Float)
-    category = Column(String(50))
+    category = Column(String(100))
     date = Column(Date)
